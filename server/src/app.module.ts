1f62ee219f207798baafc3cd7a19daa284febca6
Anton Gunnarsson <gson.anton@hotmail.com>
2020-12-19 15:03:23 +0100

    Add env vars to server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import * as Joi from '@hapi/joi'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AppGateway } from './app.gateway'
@@ -7,15 +9,36 @@ import { Score } from './score.entity'
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '',
-      database: 'testdb',
-      entities: [Score],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USER: Joi.string().required(),
+        DB_PASSWORD: Joi.string().allow(''),
+        DB_NAME: Joi.string().required(),
+        REDIS_HOST: Joi.string().required(),
+        REDIS_PORT: Joi.number().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get('DB_USER'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_NAME'),
+          entities: [Score],
+          synchronize: true,
+        }
+      },
+      inject: [ConfigService],
     }),
     TypeOrmModule.forFeature([Score]),
   ],
--- server/src/redis-io-adapter.ts
+++ server/src/redis-io-adapter.ts
@@ -1,15 +1,30 @@
 import { createAdapter } from 'socket.io-redis'
 import { RedisClient } from 'redis'
 import { IoAdapter } from './io-adapter'
-
-const pubClient = new RedisClient({ host: 'localhost', port: 6379 })
-const subClient = pubClient.duplicate()
+import { ConfigService } from '@nestjs/config'
+import { INestApplication } from '@nestjs/common'
 
 export class RedisIoAdapter extends IoAdapter {
+  private pubClient: RedisClient
+  private subClient: RedisClient
+
+  constructor(app: INestApplication) {
+    super(app)
+
+    const configService: ConfigService = app.get(ConfigService)
+    this.pubClient = new RedisClient({
+      host: configService.get('REDIS_HOST'),
+      port: configService.get<number>('REDIS_PORT'),
+    })
+    this.subClient = this.pubClient.duplicate()
+  }
+
   createIOServer(port: number, options?: any): any {
     const server = super.createIOServer(port, options)
 
-    server.adapter(createAdapter({ pubClient, subClient }))
+    server.adapter(
+      createAdapter({ pubClient: this.pubClient, subClient: this.subClient }),
+    )
     return server
   }
 }
